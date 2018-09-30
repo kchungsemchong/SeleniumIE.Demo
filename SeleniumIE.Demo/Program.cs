7aed634feed070b695f079485a6c98960c62c02e
kchungsemchong <kchungsemchong@gmail.com>
2018-09-30 12:08:02 +0400

    InitialCommit

--- SeleniumIE.Demo/Program.cs
+++ SeleniumIE.Demo/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using System;
 
 namespace SeleniumIE.Demo
 {
@@ -10,6 +8,10 @@ namespace SeleniumIE.Demo
 	{
 		static void Main(string[] args)
 		{
+			using (IWebDriver driver = new InternetExplorerDriver(@"Dependencies"))
+			{
+				driver.Navigate().GoToUrl("https://google.com");
+			}
 		}
 	}
 }
